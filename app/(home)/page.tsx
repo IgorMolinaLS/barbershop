69b38711a401ef1eb188a2a9ed673db087b96890
Igor Molina <igoramil452@gmai.com>
2024-08-06 13:45:44 -0300

    feat: add initial search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-4">
+        <h2 className="text-xl font-bold">Olá {}!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   );
 }
