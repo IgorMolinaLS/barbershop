074a942a7d00fdebd3433e6f1bec1c20179ffcc6
Igor Molina <igoramil452@gmai.com>
2024-08-12 15:41:55 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent } from "./ui/card";
+
+//todo: componentizar
+interface BookingSummaryProps {}
+
+const BookingSummary = () => {
+  return <></>;
+};
+
+export default BookingSummary;
